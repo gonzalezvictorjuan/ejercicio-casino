e00b3296f65343f89efe46dc2b12bf6f18024196
karinadesposito <110677129+karinadesposito@users.noreply.github.com>
2023-03-08 13:01:16 -0300

    prueba matrix

---
+++ Desktop/proyectos/ejercicios/MATRIZ/app.js
@@ -0,0 +1,9 @@
+var matrix = new Array(3);
+matrix[0] = new Array(1, 2, 3);
+matrix[1] = new Array(4, 5, 6);
+matrix[2] = new Array(7, 8, 9);
+console.table(matrix);
+function consultarMatrix(x, y) {
+    var consultarMatrix = matrix.values();
+}
+console.log(matrix.values(1, 2));
